6f48b2dedcea079391dd8a44a7b8e699f2ba5474
RomainCpg <123554824+RomainCpg@users.noreply.github.com>
2025-01-19 21:09:40 +0100

    add comment on utils.py

--- game/utils.py
+++ game/utils.py
@@ -7,11 +7,13 @@ food = 3
 days = 0
 events = [event1, event2, event3, event4, event5, event6, event7, event8, event9, event10, event11, event12]
 
+# Display an exit message when exiting the game
 def exitgame():
     print(separator)
     print("See you next time!")
     quit()
 
+# Display the time of the day
 def givetime():
     global hours
     print(separator)
@@ -19,7 +21,8 @@ def givetime():
         print(f"It's {hours} AM")
     else:
         print(f"It's {hours - 12} PM")
-
+        
+# Display the food of the player
 def givefood():
     print(f"Food: {food}")
 
